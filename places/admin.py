6331d528708ecbdeb3052b74eb9df4b6234eb76f
darya-ko00 <dasha.kon.060203@gmail.com>
2025-10-17 13:43:17 +0300

    add model for image

--- places/admin.py
+++ places/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
-from places.models import Place
+from places.models import Place, Image
 
 
 @admin.register(Place)
 class AdminPlace(admin.ModelAdmin):
-    list_display = ('title', )
+    list_display = ("title", )
     fieldsets = (
         (None, {
-            "fields": ('title', 'description_short',
-                       'description_long', 'lng', 'lat'),
+            "fields": ("title", "description_short",
+                       "description_long", "lng", "lat"),
         }),
     )
 
+@admin.register(Image)
+class AdminImage(admin.ModelAdmin):
+    raw_id_fields = ["place"]
--- places/models.py
+++ places/models.py
@@ -4,8 +4,16 @@ class Place(models.Model):
     title = models.CharField(verbose_name="Название", max_length=80, blank=True)
     description_short = models.TextField(verbose_name="Краткое описание", max_length=300, blank=True)
     description_long = models.TextField(verbose_name="Описание", blank=True)
-    lng = models.DecimalField(verbose_name='Долгота', max_digits=20, decimal_places=17)
-    lat = models.DecimalField(verbose_name='Шировата', max_digits=20, decimal_places=17)
+    lng = models.DecimalField(verbose_name="Долгота", max_digits=20, decimal_places=17)
+    lat = models.DecimalField(verbose_name="Шировата", max_digits=20, decimal_places=17)
 
     def __str__(self) -> str:
         return self.title
+
+class Image(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name="Место на карте", related_name="images")
+    image = models.ImageField(verbose_name="Картинка", upload_to="media/", blank=True)
+    number = models.PositiveIntegerField(verbose_name="Номер", default=1, db_index=True)
+
+    def __str__(self):
+        return f"#{self.number} - {self.place.title}"
