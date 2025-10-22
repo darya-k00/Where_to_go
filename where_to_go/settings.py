475c6479b1b1b530366edd255d7a17f182b5187e
darya-k00 <dasha.kon.060203@gmail.com>
2025-10-22 16:06:30 +0300

    Update settings.py

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -94,7 +94,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "places/static"),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -102,4 +102,5 @@ MEDIA_URL = '/media/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
